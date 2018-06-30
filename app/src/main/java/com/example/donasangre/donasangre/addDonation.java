a3188308d8d584326043dbf5b38976d8947440f8
Erick Mex <erick00mex@gmail.com>
2018-06-30 04:07:08 -0500

    Database

--- app/src/main/java/com/example/donasangre/donasangre/addDonation.java
+++ app/src/main/java/com/example/donasangre/donasangre/addDonation.java
@@ -2,12 +2,38 @@ package com.example.donasangre.donasangre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Date;
+
 
 public class addDonation extends AppCompatActivity {
+
+    database db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_donation);
+        final EditText ltDonated = (EditText) findViewById(R.id.txtLtDonated);
+        Button addLtDonated = (Button) findViewById(R.id.btnAddDonation);
+        db = new database(this);
+
+        addLtDonated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean donated = db.insertActivity(new Date().toLocaleString(), Double.parseDouble(ltDonated.getText().toString()));
+                if (donated) {
+                    Toast.makeText(addDonation.this, "Success" , Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(addDonation.this, "¡Ooops! Usuario no registrado", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/donasangre/donasangre/database.java
+++ app/src/main/java/com/example/donasangre/donasangre/database.java
@@ -2,9 +2,12 @@ package com.example.donasangre.donasangre;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class database extends SQLiteOpenHelper {
@@ -17,10 +20,10 @@ public class database extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("create table user" +
-        "(id integer primary key, name text, pass text, phone text, mail text, birthday date)");
+        "(id integer primary key, name text, pass text, phone text, mail text, birthday text)");
 
         sqLiteDatabase.execSQL("create table activity" +
-                "(id_activity integer primary key, donate_day Date, idUser int)");
+                "(id_activity integer primary key, donate_day text, ltDonated double)");
     }
 
     @Override
@@ -30,24 +33,24 @@ public class database extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
-    public boolean insertUser(String name, String pass, String phone, String mail, Date birthday) {
+    public boolean insertUser(String name, String pass, String phone, String mail, String birthday) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("name", name);
         contentValues.put("pass", pass);
         contentValues.put("phone", phone);
         contentValues.put("mail", mail);
-        contentValues.put("birthday", String.valueOf(birthday));
+        contentValues.put("birthday", birthday);
         db.insert("user", null, contentValues);
         return true;
     }
 
-    public boolean insertActivity(Date donate_day, int idUser) {
+    public boolean insertActivity(String donate_day, double ltDonated) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("donate_day", String.valueOf(donate_day));
-        contentValues.put("idUser", idUser);
-        db.insert("Activity", null, contentValues);
+        contentValues.put("donate_day", donate_day);
+        contentValues.put("ltDonated", ltDonated);
+        db.insert("activity", null, contentValues);
         return true;
     }
 
@@ -60,5 +63,47 @@ public class database extends SQLiteOpenHelper {
         contentValues.put("mail", mail);
         db.update("user", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
         return true;
+    }
+
+    public int numberOfDonatedRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, "activity");
+        return numRows;
+    }
+
+    public boolean getUser(String mail, String password) {
+        ArrayList<String> array_list = new ArrayList<String>();
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from user where pass like '"+password+"' AND mail like '"+ mail+"'", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            array_list.add(res.getString(res.getColumnIndex("name")));
+            res.moveToNext();
+        }
+        if (array_list.size() > 0 ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public ArrayList<ArrayList> getAllDonations() {
+        ArrayList<ArrayList> array_list = new ArrayList<ArrayList>();
+
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from activity", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            ArrayList<String> array_aux = new ArrayList<String>();
+            array_aux.add(res.getString(res.getColumnIndex("donate_day")));
+            array_aux.add(res.getString(res.getColumnIndex("ltDonated")));
+            array_list.add(array_aux);
+            res.moveToNext();
+        }
+        return array_list;
     }
 }
